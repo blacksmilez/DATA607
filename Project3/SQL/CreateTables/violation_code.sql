59332cc77ad777f85d5b562e72ec58f9fe0928ab
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 15:40:41 -0400

    Update violation_code.sql

--- Project3/SQL/CreateTables/violation_code.sql
+++ Project3/SQL/CreateTables/violation_code.sql
@@ -1,1 +1,5 @@
-
+create table tbl_violation_code (
+    violation_code     int        not null,
+    violation_code_5   varchar(5) not null,
+    primary key(violation_code)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
