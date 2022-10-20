7c66d7309c2910c8ed0e8fcfcdf1bb765cc14480
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 15:56:57 -0400

    Update violation_description_code.sql

--- Project3/SQL/CreateTables/violation_description_code.sql
+++ Project3/SQL/CreateTables/violation_description_code.sql
@@ -1,4 +1,4 @@
-create table tbl_violation_description_code (
+create table if not exists tbl_violation_description_code (
     violation_description_code   int           not null,
     violation_description        varchar(1000) not null,
     primary key(violation_description_code)
