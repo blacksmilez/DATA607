0e9cd2ccbca2446bddf973215c504c920a551509
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 15:38:19 -0400

    Create critical_flag_code.sql

---
+++ Project3/SQL/CreateTables/critical_flag_code.sql
@@ -0,0 +1,5 @@
+create table tbl_critical_flag_code (
+    critical_flag_code   int         not null,
+    critical_flag        varchar(15) not null,
+    primary key(critical_flag_code)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
