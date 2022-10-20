865a9678ae60c766f127c3904a2374151abc9555
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 15:51:30 -0400

    Create inspection_type.sql

---
+++ Project3/SQL/CreateTables/inspection_type.sql
@@ -0,0 +1,7 @@
+create table tbl_inspection_type (
+camis              varchar(8) not null,
+  inspection_date    date,
+inspection_code    int not null,
+  foreign key(camis, inspection_date) references tbl_inspection_result(camis, inspection_date),
+  foreign key(inspection_code) references tbl_inspection_code(inspection_code)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
