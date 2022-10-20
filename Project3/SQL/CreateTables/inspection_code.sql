d57e80e4cfc7df6b621a284a63fdb35bd90af1be
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 15:55:47 -0400

    Update inspection_code.sql

--- Project3/SQL/CreateTables/inspection_code.sql
+++ Project3/SQL/CreateTables/inspection_code.sql
@@ -1,4 +1,4 @@
-create table tbl_inspection_code (
+create table if not exists tbl_inspection_code (
     inspection_code    int         not null,
     inspection_type    varchar(70) not null,
     primary key(inspection_code)
