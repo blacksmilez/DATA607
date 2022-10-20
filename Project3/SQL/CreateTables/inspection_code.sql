4092e32bea8d60b14d0eafe498f35cd4083a62f6
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 17:51:45 -0400

    Update inspection_code.sql

--- Project3/SQL/CreateTables/inspection_code.sql
+++ Project3/SQL/CreateTables/inspection_code.sql
@@ -1,5 +1,6 @@
 create table if not exists tbl_inspection_code (
-    inspection_code    int         not null,
+--    inspection_code    int         not null,
+    inspection_code    varchar(2)  not null,
     inspection_type    varchar(70) not null,
     primary key(inspection_code)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
