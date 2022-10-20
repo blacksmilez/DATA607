4ef38a05ea49418ce86066060bee5f307bc162f5
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 17:50:24 -0400

    Update critical_flag_code.sql

--- Project3/SQL/CreateTables/critical_flag_code.sql
+++ Project3/SQL/CreateTables/critical_flag_code.sql
@@ -1,5 +1,6 @@
 create table if not exists tbl_critical_flag_code (
-    critical_flag_code   int         not null,
+--    critical_flag_code   int         not null,
+    critical_flag_code   varchar(1)  not null,
     critical_flag        varchar(15) not null,
     primary key(critical_flag_code)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
