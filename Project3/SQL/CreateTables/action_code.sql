b355ee376c953dd2bc70dcda908c6982e0ded513
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 17:48:07 -0400

    Update action_code.sql

--- Project3/SQL/CreateTables/action_code.sql
+++ Project3/SQL/CreateTables/action_code.sql
@@ -1,5 +1,6 @@
 create table if not exists tbl_action_code (
-    action_code    int unsigned not null,
+--    action_code    int unsigned not null,
+    action_code    varchar(2)   not null,
     `action`       varchar(150) not null,
     primary key(action_code)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
