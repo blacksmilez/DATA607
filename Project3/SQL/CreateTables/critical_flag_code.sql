2f55d9ba5070f5ebb455706625de27d48d718938
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 15:54:54 -0400

    Update critical_flag_code.sql

--- Project3/SQL/CreateTables/critical_flag_code.sql
+++ Project3/SQL/CreateTables/critical_flag_code.sql
@@ -1,4 +1,4 @@
-create table tbl_critical_flag_code (
+create table if not exists tbl_critical_flag_code (
     critical_flag_code   int         not null,
     critical_flag        varchar(15) not null,
     primary key(critical_flag_code)
