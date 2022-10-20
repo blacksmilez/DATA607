88f4c54d1cce85e2af3dea3a7edfd4383511daaa
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 15:01:55 -0400

    Create drop_tables.sql

---
+++ Project3/drop_tables.sql
@@ -0,0 +1,14 @@
+/* drop tables */
+drop table tbl_inspection_type;
+drop table tbl_violation;
+drop table tbl_inspection_result;
+drop table tbl_dba;
+drop table tbl_building;
+drop table tbl_cuisine_code;
+drop table tbl_inspection_code;
+drop table tbl_action_code;
+drop table tbl_grade_code;
+drop table tbl_violation_code;
+drop table tbl_violation_description_code;
+drop table tbl_critical_flag_code;
+drop table tbl_boro;
