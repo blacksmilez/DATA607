8ac444344b84f78440323f2808cc826e90936344
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 15:02:19 -0400

    Create truncate_tables.sql

---
+++ Project3/truncate_tables.sql
@@ -0,0 +1,14 @@
+/* truncate tables */
+truncate table tbl_inspection_type;
+truncate table tbl_violation;
+truncate table tbl_inspection_result;
+truncate table tbl_dba;
+truncate table tbl_building;
+truncate table tbl_cuisine_code;
+truncate table tbl_inspection_code;
+truncate table tbl_action_code;
+truncate table tbl_grade_code;
+truncate table tbl_violation_code;
+truncate table tbl_violation_description_code;
+truncate table tbl_critical_flag_code;
+truncate table tbl_boro;
