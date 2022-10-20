5522a810920e5691335b4f265d9dc06cd43d205c
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 19:34:39 -0400

    Update boro.sql

--- Project3/SQL/CreateTables/boro.sql
+++ Project3/SQL/CreateTables/boro.sql
@@ -1,5 +1,5 @@
 create table if not exists tbl_boro (
-    boro_code   int unsigned not null,
+    boro_code   varchar(1)   not null,
     boro_name   varchar(20)  not null,
     primary key(boro_code)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
