061aa4425e2e14709cfdd7a6837c4527354e618e
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 15:55:33 -0400

    Update grade_code.sql

--- Project3/SQL/CreateTables/grade_code.sql
+++ Project3/SQL/CreateTables/grade_code.sql
@@ -1,4 +1,4 @@
-create table tbl_grade_code (
+create table if not exists tbl_grade_code (
     grade_code    varchar(1) not null,
     grade         varchar(5) not null,
     primary key(grade_code)
