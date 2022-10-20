d75f8b761704d1e4c547e9858cbf68f10134a4ae
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 15:52:23 -0400

    Update inspection_type.sql

--- Project3/SQL/CreateTables/inspection_type.sql
+++ Project3/SQL/CreateTables/inspection_type.sql
@@ -1,7 +1,7 @@
 create table tbl_inspection_type (
-camis              varchar(8) not null,
-  inspection_date    date,
-inspection_code    int not null,
-  foreign key(camis, inspection_date) references tbl_inspection_result(camis, inspection_date),
-  foreign key(inspection_code) references tbl_inspection_code(inspection_code)
+    camis              varchar(8) not null,
+    inspection_date    date       not null,
+    inspection_code    int        not null,
+    foreign key(camis, inspection_date) references tbl_inspection_result(camis, inspection_date),
+    foreign key(inspection_code) references tbl_inspection_code(inspection_code)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
