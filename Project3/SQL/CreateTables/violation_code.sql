3d64ef11a98e17c3c082466f00e4413d051dd9da
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 21:00:47 -0400

    Update violation_code.sql

--- Project3/SQL/CreateTables/violation_code.sql
+++ Project3/SQL/CreateTables/violation_code.sql
@@ -1,5 +1,4 @@
 create table if not exists tbl_violation_code (
---    violation_code     int        not null,
     violation_code     varchar(3) not null,
     violation_code_5   varchar(5) not null,
     primary key(violation_code)
