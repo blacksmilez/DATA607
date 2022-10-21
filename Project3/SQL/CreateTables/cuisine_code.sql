f3fee28c6c9908dfbf944f2b3e2595f517b79b93
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 21:54:41 -0400

    Update cuisine_code.sql

--- Project3/SQL/CreateTables/cuisine_code.sql
+++ Project3/SQL/CreateTables/cuisine_code.sql
@@ -1,5 +1,1 @@
-create table if not exists tbl_cuisine_code (
-    cuisine_code          varchar(3)  not null,
-    cuisine_description    varchar(50) not null,
-    primary key(cuisine_code)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
