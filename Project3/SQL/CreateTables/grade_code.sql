035565dd954f361c42365bff987f68979c418573
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 20:17:37 -0400

    Update grade_code.sql

--- Project3/SQL/CreateTables/grade_code.sql
+++ Project3/SQL/CreateTables/grade_code.sql
@@ -1,5 +1,5 @@
 create table if not exists tbl_grade_code (
     grade_code    varchar(1) not null,
-    grade         varchar(5) not null,
+    grade         varchar(100) not null,
     primary key(grade_code)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
