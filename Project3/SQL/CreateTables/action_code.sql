f6b48666577f15ade8732339d101f88faa990ef4
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 21:23:36 -0400

    Update action_code.sql

--- Project3/SQL/CreateTables/action_code.sql
+++ Project3/SQL/CreateTables/action_code.sql
@@ -1,5 +1,4 @@
 create table if not exists tbl_action_code (
---    action_code    int unsigned not null,
     action_code    varchar(2)   not null,
     `action`       varchar(150) not null,
     primary key(action_code)
