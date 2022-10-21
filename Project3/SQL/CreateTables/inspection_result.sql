45760c68ae5a9e3598a1745a910e78143546b77f
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 22:30:20 -0400

    Update inspection_result.sql

--- Project3/SQL/CreateTables/inspection_result.sql
+++ Project3/SQL/CreateTables/inspection_result.sql
@@ -1,15 +1,1 @@
-create table if not exists tbl_inspection_result (
-    camis              varchar(8)       not null,
-    inspection_date    date             not null,
-    action_code        varchar(2)           null,
-    score              int                  null,
-    grade_code         varchar(1)           null,
-    grade_date         date                 null,
-    record_date        date             not null,
-    cuisine_code       varchar(3)        not null,
-    primary key(camis, inspection_date),
-    foreign key(camis) references tbl_dba(camis),
-    foreign key(action_code) references tbl_action_code(action_code),
-    foreign key(grade_code) references tbl_grade_code(grade_code),
-    foreign key(cuisine_code) references tbl_cuisine_code(cuisine_code)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
