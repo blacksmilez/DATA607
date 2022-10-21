4e79b27f38963a60c876c332b82064921de076f0
Ted <43358528+blacksmilez@users.noreply.github.com>
2022-10-20 22:14:16 -0400

    Update inspection_type.sql

--- Project3/SQL/CreateTables/inspection_type.sql
+++ Project3/SQL/CreateTables/inspection_type.sql
@@ -1,7 +1,6 @@
 create table if not exists tbl_inspection_type (
     camis              varchar(8) not null,
-    inspection_date    date       not null,
---    inspection_code    int        not null,
+    inspection_date    date           null,
     inspection_code    varchar(2) not null,
     foreign key(camis, inspection_date) references tbl_inspection_result(camis, inspection_date),
     foreign key(inspection_code) references tbl_inspection_code(inspection_code)
